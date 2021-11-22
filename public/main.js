de19b34b202994641e1f28769046e856d17ce7a6
mirandanilhag <miranda_nilhag@hotmail.com>
2021-11-22 15:40:15 +0100

    fixed problems with layout

--- public/main.js
+++ public/main.js
@@ -1,2 +1,19 @@
-window.addEventListener('load', main);
+let countDownDate = new Date('May 31, 2023 12:00:00').getTime();
 
+let x = setInterval(function() {
+    let now = new Date().getTime();
+
+    let distance = countDownDate - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 *60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
