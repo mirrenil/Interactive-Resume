4d10a1c168cf3c0d7103c66d85c22d3b8ee8e4ab
mirandanilhag <miranda_nilhag@hotmail.com>
2021-11-23 10:50:45 +0100

    trying a new function, not working yet

--- public/main.js
+++ public/main.js
@@ -1,31 +1,59 @@
-let countDownDate = new Date('May 31, 2023 12:00:00').getTime();
+window.addEventListener('load', main);
 
-let x = setInterval(function() {
-    let now = new Date().getTime();
+/** Program starts */
+function main() {
+  countDown();
+  clickToScroll();
+  clickToScrollDown();
+}
+/** This is a function which counts down until graduation */
+function countDown() {
+  let countDownDate = new Date("May 31, 2023 12:00:00").getTime();
+
+  let x = setInterval(function () {
+    const now = new Date().getTime();
 
     let distance = countDownDate - now;
 
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 *60 * 24)) / (1000 * 60 * 60));
+    let hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    document.getElementById("count-down").innerHTML =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  }, 1000);
+}
 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-}, 1000);
+/** If you click on Contact the page will scroll down to the footer where my contact info is */
+function clickToScroll() {
 
+  const contact = document.querySelector("footer");
 
-/* function scrollDown() {
-    let arrow = document.getElementById('arrow');
+  contact.addEventListener('click', function() {
+    window.scrollTo({behavior: "smooth"});
+  });
+}
+/** If you click the arrow the page scrolls down to the main content */
+function clickToScrollDown() {
+
+    const arrow = document.querySelector("main");
+
     arrow.addEventListener('click', function() {
-        window.scrollTo() {
-            top: 700;,
-            left: 700;,
-            behavior: 'smooth';
-        }
+        window.scrollTo({behavior: "smooth"});
+    });
+    
+}
+
+function hideImg(contactInfo, img) {
+    let div = document.getElementById(contactInfo);
+    if (div.style.display === "none") {
+        div.style.display = "block";
+        img.src = "/public/assets/IMG_0978.jpeg"
+    } else if (div.style.display === "block") {
+        div.style.display = "none";
+        img.src = "/public/assets/IMG_0978.jpeg"
     }
-} */
+}
